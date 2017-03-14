9a75fa81a0cdb71a1db68f214e95e8f134855fbe
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2017-03-14 20:58:13 +0300

    Initial Commit

---
+++ geekTests/geekTests.swift
@@ -0,0 +1,36 @@
+//
+//  geekTests.swift
+//  geekTests
+//
+//  Created by Влад Купряков on 14.03.17.
+//  Copyright © 2017 Влад Купряков. All rights reserved.
+//
+
+import XCTest
+@testable import geek
+
+class geekTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
